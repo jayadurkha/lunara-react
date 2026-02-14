d55ba8d659355f691ee84961cf36db16db9998c7
jayadurkha <jayadurkha@gmail.com>
2026-02-14 12:52:37 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
       <div className="container">
-        <a className="navbar-brand d-flex align-items-center" href="/">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
           <img src={logo} alt="Lunara" className="logo-img" />
-        </a>
+        </Link>
 
         <h1 className="mb-0">Lunara</h1>
 
@@ -22,16 +23,16 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto fw-semibold">
             <li className="nav-item">
-              <a className="nav-link active" href="/">Home</a>
+              <Link className="nav-link active" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/collections">Collection</a>
+              <Link className="nav-link" to="/collections">Collection</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">About</a>
+              <Link className="nav-link" to="/about">About</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/Contact">Contact</a>
+              <Link className="nav-link" to="/Contact">Contact</Link>
             </li>
           </ul>
         </div>
